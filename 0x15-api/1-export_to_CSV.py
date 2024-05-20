873701fc49c0392649e64ffa33ae79cfaa29e742
Mahuyon <latomv@gmail.com>
2024-05-20 16:54:03 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that uses JSONPlaceholder API to get information """
+""" Script that uses JSONPlaceholder API to get information about employee """
 import csv
 import requests
 import sys
